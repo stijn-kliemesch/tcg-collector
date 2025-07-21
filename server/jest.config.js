b1feabc4bbb50c9e23366963ebb1857145851674
stijn-kliemesch <216340744+stijn-kliemesch@users.noreply.github.com>
2025-07-21 23:11:35 +0000

    added mocked tests

---
+++ server/jest.config.js
@@ -0,0 +1,17 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: [
+    '**/__tests__/**/*.(spec|test).(ts|tsx|js)',
+    '**/?(*.)+(spec|test).(ts|tsx|js)'
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  setupFiles: ['<rootDir>/src/__tests__/setup.ts']
+};
---
+++ server/src/__tests__/setup.ts
@@ -0,0 +1,2 @@
+// Mock environment variables
+process.env.POKEMON_TCG_API_KEY = 'test-api-key';
