7b56ff2be6565deeec64670ae6b056bd5ddc6e9a
stijn-kliemesch <216340744+stijn-kliemesch@users.noreply.github.com>
2025-07-24 19:14:25 +0000

    updated checrrypick

--- cherrypick.sh
+++ cherrypick.sh
@@ -1,6 +1,12 @@
-set -e
+set -ex
+
+git checkout main
 
 git log wip --pretty=format:"%H" --not main | cat - <(echo "") | tac | while read hash || [[ -n $hash ]]; do
     git cherry-pick -n $hash
     git commit -m "$(git log --format=%B -n 1 $hash | xargs)"
 done
+
+git checkout wip
+
+git reset --hard main
